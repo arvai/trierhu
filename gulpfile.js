6c17d9b15bf1e8fb86f958d06fcfba9b596eaa41
nlite <arvai.zsolt@gmail.com>
2016-04-20 23:35:49 +0200

    Basic layout, add watcher task

--- gulpfile.js
+++ gulpfile.js
@@ -6,17 +6,21 @@ var sourcemaps = require('gulp-sourcemaps');
 var webpack = require('webpack-stream');
 var plugin = require('webpack');
 
-gulp.task('default', ['bundle', 'sass']);
+// Default task, full build process
+gulp.task('default', ['bundle', 'sass', 'copy-static']);
 
+// SASS build
 gulp.task('sass', function() {
 	return gulp.src('src/scss/*.scss')
 		.pipe(sourcemaps.init())
 		.pipe(sass({outputStyle: 'compressed'}))
 		.pipe(sourcemaps.write())
+
 		.pipe(concat('site.css'))
 		.pipe(gulp.dest('bin'))
 });
 
+// ES6 - JS build process
 gulp.task("es6", function () {
   return gulp.src("src/js/*.js")
     .pipe(babel({
@@ -26,7 +30,7 @@ gulp.task("es6", function () {
     .pipe(gulp.dest("bin"));
 });
 
-
+// Create bundle - webpack
 gulp.task('bundle', ['es6'], function() {
 	return gulp.src('bin/es6.js')
 		.pipe(webpack({
@@ -41,4 +45,19 @@ gulp.task('bundle', ['es6'], function() {
 			]
 		}))
 		.pipe(gulp.dest('bin/'));
+});
+
+// Watcher task for css and js
+gulp.task('watch', function() {
+	gulp.watch('src/scss/*.scss', ['sass']);
+	gulp.watch('src/js/*.js', ['bundle']);
+});
+
+// Copy static files to bin directory
+gulp.task('copy-static', function() {
+    gulp.src('./index.html')
+    .pipe(gulp.dest('./bin'));
+
+	gulp.src('./src/img/*.*')
+    .pipe(gulp.dest('./bin'));
 });
--- src/js/index.js
+++ src/js/index.js
@@ -2,13 +2,27 @@
 import {moment} from 'moment';
 
 window.TIMETABLE = [
-	'12:00'
+	'08:21',
+	'09:21',
+	'10:21',
+	'11:21',
+	'12:21',
+	'13:21',
+	'14:21',
+	'15:21',
+	'16:21',
+	'16:36',
+	'16:51',
+	'17:06',
+	'17:21'
 ];
 
 class index {
 
 	constructor() {
-		console.log("Hello world !");
+		console.log("Hello world !!");
 	}
 
 }
+
+new index();
