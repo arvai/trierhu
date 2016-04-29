887f761c0d3a13c49de4081e7af4c87906279b63
nlite <arvai.zsolt@gmail.com>
2016-04-29 22:25:19 +0200

    Basics of Mongoose

--- serverapp/server.js
+++ serverapp/server.js
@@ -1,22 +1,35 @@
 var express = require('express');
+var mongoose = require('mongoose');
+
 var app = express();
+
+mongoose.connect('mongodb://localhost:27017/trierhu');
+
+var db = mongoose.connection;
+
+// Creating schema for Journeys mongo collection
+var journeysSchema = new mongoose.Schema({
+	bus: String
+});
+
+var Journeys = mongoose.model('Journeys', journeysSchema);
 
 // Default 
 app.get('/', function (req, res) {
-	res.send('Hello World!');
+	res.send('ExpressJS Backend is up and running!');
 });
 
- 
-// Shut down myself.
-// Before deploying, the server should be shutted down...
-app.get('/shutdown', function(req, res) {
-	var itsMe = req.connection.remoteAddress == '::1';
+// Get timetable 
+app.get('/get-timetable', function (req, res) {
+	Journeys.find(function(err, journeys) {
+		if (err) return console.error(err);
+		res.send(journeys);
+	});
+/*	var obj = {bus: '11'};
 
-	if (itsMe) {
-		console.log('Closing app.');
-		process.exit(1);
-	}
-});
+	res.setHeader('Content-Type', 'application/json');
+	res.send(JSON.stringify(obj));*/
+}); 
 
 var server = app.listen(3000, function () {
 	console.log('ServerApp listening on port 3000');
