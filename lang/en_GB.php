b0fa8f97ac12afe6cc6a276a17cbb934bf414e14
Zsolt Árvai <arvai.zsolt@gmail.com>
2016-10-28 20:03:14 +0200

    Google

--- lang/en_GB.php
+++ lang/en_GB.php
@@ -2,7 +2,7 @@
 return array(
 	'meta'                               => array(
 		'title'       => 'Trier.hu - When does my bus departs?',
-		'description' => 'A dead simple public transport web app to check whether should you rush to the closest bus stop after working hours, or not. Optimized for habits of people who work in Kirchberg, Luxembourg, and live in Trier, Germany.',
+		'description' => 'For those who commutes by the Luxembourg-Trier Bus 118. A dead simple public transport web app to check whether should you rush to the closest bus 118 stop in Luxembourg after working hours, or not. Optimized for habits of people who work in Kirchberg, Luxembourg, and live in Trier, Germany.',
 	),
 	'the_next_bus'                       => 'The next bus 118',
 	'goes_to_trier'                      => 'goes to Trier',
